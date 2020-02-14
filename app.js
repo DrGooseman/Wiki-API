de5771ca72045b7684fcf06f9ca3115641173757
DrGooseman <jamesjquinn12@gmail.com>
2020-02-14 23:08:23 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB", {
+  useNewUrlParser: true
+});
+
+const articleSchema = {
+  title: String,
+  content: String
+};
+
+const article = mongoose.model("Article", articleSchema);
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running on port 3000");
+});
